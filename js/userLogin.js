ca94d45a0091eb81f5cc6e3ca2b32621a3fde921
Priyanshu Gupta <56040132+priyanshu411@users.noreply.github.com>
2022-02-19 16:25:21 +0530

    user login js

---
+++ js/userLogin.js
@@ -0,0 +1,38 @@
+// import firebase sdk
+import { app } from './firebase.js';
+import { GoogleAuthProvider ,getAuth,signInWithPopup, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-auth.js";
+
+const provider = new GoogleAuthProvider(app);
+const auth = getAuth(app);
+document.getElementById("login").addEventListener("click",loginWithGoogle);
+console.log(app);
+window.onload = removePreloader;
+
+
+
+function loginWithGoogle(){
+
+    signInWithPopup(auth, provider)
+    .then((result) => {
+        if(result.user)
+             window.location = "quiz.html";
+    }).catch((error) => {
+      alert(errorMessage +"\n"+"email :"+ error.email)
+    });
+
+}
+
+
+
+// remove preloader
+function removePreloader(){
+
+    onAuthStateChanged(auth, (user) => {
+        if (user && user.providerData[0].providerId.localeCompare("google.com") === 0) {
+            window.location = "quiz.html";
+        } else {
+            document.getElementById("loader").remove();
+        }
+      });
+
+}
